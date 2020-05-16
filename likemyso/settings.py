8301b01d7be8294c1f3e78eaf0d7af1acbbf2eb1
iwpnd <legionaerr@googlemail.com>
2020-05-16 16:47:40 +0200

    add environment variables to build/test workflow

--- likemyso/settings.py
+++ likemyso/settings.py
@@ -8,6 +8,6 @@ load_dotenv(find_dotenv())
 USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
 SETTINGSFILE = os.getenv("SETTINGSFILE", "config.json")
-USERS_TO_LIKE = [i for i in os.environ.get("USERS_TO_LIKE").split(",")] or None
+USERS_TO_LIKE = [i for i in os.getenv("USERS_TO_LIKE").split(",")]
 LAST_N_PICTURES = int(os.getenv("LAST_N_PICTURES", 5))
 TIME_SLEEP_BETWEEN_CALLS = int(os.getenv("TIME_SLEEP_BETWEEN_CALLS", 10))
