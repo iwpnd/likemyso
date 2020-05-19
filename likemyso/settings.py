a64d4f01457cc970793c483b927a13fa362a1818
iwpnd <legionaerr@googlemail.com>
2020-05-20 01:01:00 +0200

    ditched weird way to parse dotenv

--- likemyso/settings.py
+++ likemyso/settings.py
@@ -8,6 +8,6 @@ load_dotenv(find_dotenv())
 INSTAGRAM_USERNAME = os.getenv("INSTAGRAM_USERNAME", "default")
 INSTAGRAM_PASSWORD = os.getenv("INSTAGRAM_PASSWORD", "default")
 SETTINGSFILE = os.getenv("SETTINGSFILE", "config.json")
-USERS_TO_LIKE = [i for i in os.getenv("USERS_TO_LIKE", "").split(",")]
+USERS_TO_LIKE = os.getenv("USERS_TO_LIKE", "").split(",")
 LAST_N_PICTURES = int(os.getenv("LAST_N_PICTURES", 5))
 TIME_SLEEP_BETWEEN_CALLS = int(os.getenv("TIME_SLEEP_BETWEEN_CALLS", 10))
